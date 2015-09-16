44c37c49c835a230b3bbe06e67b6ff783e6f60b8
Micha Rautenberg <rautenberg@in-silico.ch>
2015-09-16 17:26:09 +0200

    add route get /model

--- application.rb
+++ application.rb
@@ -12,6 +12,10 @@ use Rack::Cors do |config|
       :max_age => 0
   end
 end
+before do
+  @accept = request.env['HTTP_ACCEPT']
+  response['Content-Type'] = @accept
+end
  
 # route to swagger API file
 get "/api/api.json" do
@@ -19,4 +23,17 @@ get "/api/api.json" do
   api_file = File.join("api", "api.json")
   bad_request_error "API Documentation in Swagger JSON is not implemented.", uri("/#{SERVICE}/api") unless File.exists?(api_file)
   File.read(api_file)
+end
+
+
+# Get a list of all prediction models
+# @param [Header] Accept one of text/uri-list,
+# @return [text/uri-list] list of all prediction models
+get "/model/?" do
+  @models = OpenTox::Model::Prediction.all
+  uri_list = @models.collect{|model| uri("/model/#{model.model_id}")}
+  case @accept
+  when "text/uri-list"
+    return uri_list.join("\n") + "\n"
+  end
 end
