251c2532ed714a5880fc5a314333b84a941544fd
Micha Rautenberg <rautenberg@in-silico.ch>
2015-09-16 11:15:39 +0200

    test for GET API doc

--- test/api.rb
+++ test/api.rb
@@ -1,9 +1,13 @@
 require_relative "setup.rb"
+$api_uri = "https://mr-test.in-silico.ch/api/api.json"
+
 
 class ApiTest < MiniTest::Test
 
   def test_0_api_get
-    
+    res = RestClientWrapper.get $api_uri
+    assert_equal res.code, 200
+    assert JSON.parse "#{res.body}"
   end
 
 end
--- test/setup.rb
+++ test/setup.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 
 require_relative '../../lazar/lib/lazar.rb'
-#include OpenTox
+include OpenTox
 TEST_DIR ||= File.expand_path(File.dirname(__FILE__))
 DATA_DIR ||= File.join(TEST_DIR,"data")
