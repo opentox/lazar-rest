0584ef8899fc89d861be25de62b8417a543cfa82
rautenberg <rautenberg@in-silico.ch>
2016-09-06 13:12:59 +0200

    add A&A test

---
+++ test/aa.rb
@@ -0,0 +1,23 @@
+require_relative "setup.rb"
+
+class AATest < MiniTest::Test
+
+  def test_0_login
+    res = RestClientWrapper.post(File.join($host,"aa/authenticate"),{:username=>"guest", :password => "guest"},{:Accept => "text/plain"})
+    assert_equal res.code, 200
+    assert_equal res.size, 62
+    @@token = res
+  end
+  
+  def test_1_logout
+    assert @@token
+    assert_equal @@token.size, 62
+    res = RestClientWrapper.post(File.join($host,"aa/logout"),{:subjectid=>@@token},{:Accept => "text/plain"})
+    assert_equal res.code, 200
+    assert_equal res, "Successfully logged out. \n"
+    res = RestClientWrapper.post(File.join($host,"aa/logout"),{:subjectid=>@@token},{:Accept => "text/plain"})
+    assert_equal res.code, 200
+    assert_equal res, "Logout failed.\n"
+  end  
+
+end
