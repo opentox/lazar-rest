31eccd98440dd9b28580ae9e1961ab738dc79823
gebele <gebele@in-silico.ch>
2017-06-08 11:31:45 +0000

    update for sinatra cors and swagger in one image

---
+++ lib/swagger.rb
@@ -0,0 +1,8 @@
+set :public_folder, File.join("/home/ist/swagger-ui/dist/")
+# route to swagger API file
+get "/" do
+  response['Content-Type'] = "text/html"
+  index_file = File.join("/home/ist/swagger-ui/dist/index.html")
+  #bad_request_error "API Documentation in Swagger JSON is not implemented.", uri("/#{SERVICE}/api") unless File.exists?(index_file)
+  File.read(index_file)
+end
